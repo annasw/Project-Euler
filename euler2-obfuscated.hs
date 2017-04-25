ae9ca462227694782f310966d3adee6df7fa3f68
Jesse White <jsperlingwhite@gmail.com>
2017-04-25 16:09:25 -0700

    Update euler2-obfuscated.hs
    obfuscated further

--- euler2-obfuscated.hs
+++ euler2-obfuscated.hs
@@ -1,4 +1,4 @@
 e2f = about 4 million
     where c=(<=v);bp=(t^x)+d;w=11;sk=scanl;d=n^x;s=10;t7=takeWhile;v=(4*bp);about=max;n=x^s-68;
-          fu=f(e4e)b;fk9e=filter;million=sum$t7(c)$fu;b=0:sk(+)1(b);x=2;f=fk9e;e4e=even;t=x^(5)*w;
+          fu=f(e4e)b;fk9e=filter;million=sum$t7(c)$fu;b=0:sk(+)1(b);x=2;f=fk9e;e4e(p)=p`mod`2==0;t=x^(5)*w;
           
